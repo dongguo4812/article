01bea447244a9b0bad50f52a0ae4efea917c9309
dongguo4812 <291320608@qq.com>
2021-10-24 16:11:24 +0800

    second commit

---
+++ src/main/java/com/dongguo/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.dongguo.git;
+
+/**
+ * @author Dongguo
+ * @date 2021/10/24 0024-16:09
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
