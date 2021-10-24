79e4f1ba9e6e7d917a5a1f81e133caa0e9fb1249
dongguo4812 <291320608@qq.com>
2021-10-24 16:11:51 +0800

    third commit

--- src/main/java/com/dongguo/git/GitTest.java
+++ src/main/java/com/dongguo/git/GitTest.java
@@ -8,5 +8,6 @@ package com.dongguo.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
